948bb4f0530bb433862d5f058c96e53e7b70b8c1
Nimish Lal <nlal@frontporch.com>
2021-08-10 11:04:05 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  function refreshPage() {
+    window.location.reload(false);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="center">Fiji Radio</h1>
+      <div>
+        <br></br>
+        <div className="text-center border">
+          <h1>Radio Fiji 2</h1>
+          <audio controls>
+            <source
+              src="http://peridot.streamguys.com:7150/RFTwo"
+              type="audio/mp3"
+            />
+          </audio>
+        </div>
+        <br></br>
+        <div className="text-center border">
+          <h1>Radio navtarang fiji</h1>
+          <audio controls preload="auto">
+            <source
+              src="https://stream.fijivillage.com/navtarang?ver=636650.mp3"
+              type="audio/mp3"
+            ></source>
+          </audio>
+        </div>
+        <br></br>
+        <div className="text-center border">
+          <h1>Radio Sargam</h1>
+          <audio controls preload="auto">
+            <source
+              src="https://stream.fijivillage.com/radiosargam"
+              type="audio/mp3"
+            ></source>
+          </audio>
+          <br></br>
+        </div>
+      </div>
+      <br></br>
+      <div className="text-center">
+        <button className="" onClick={refreshPage}>
+          Click to reload!
+        </button>
+      </div>
+      <div className="text-center border">
+        <h1>Bula FM Fiji</h1>
+        <audio controls>
+          <source
+            src="http://peridot.streamguys.com:7150/Bula"
+            type="audio/mp3"
+          ></source>
+        </audio>
+      </div>
+      {
+        <div className="text-center border">
+          <h1>Radio Mirchi Fiji</h1>
+          <audio width="" height="" controls>
+            <source
+              src="http://peridot.streamguys.com:7150/Mirchi"
+              type="audio/mp3"
+            />
+          </audio>
+        </div>
+      }
+      <footer className="colorFooter text-center">
+        Made By Chidoro Batkau V1.0 MixKarDo
+      </footer>
     </div>
   );
 }
